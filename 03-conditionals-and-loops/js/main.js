66dc6ab3ed870403eebf78e130c167148c0b7db4
Luis Angelo Rodrigues Jr <luisangelorjr@gmail.com>
2019-03-01 19:36:40 -0300

    Lesson 03-08 'The if else if Statemente' of '03-conditionals-and-loops' is complete

--- 03-conditionals-and-loops/js/main.js
+++ 03-conditionals-and-loops/js/main.js
@@ -1,6 +1,13 @@
 var myNum1 = 7;
 var myNum2 = 10;
 
-if (myNum1 < myNum2) {
-    alert("Javascript is easy to learn.")
+if (myNum1 < myNum2){
+    alert("Javascript is easy to learn.\nJavascript é fácil de aprender.")
+}
+
+if (myNum1 > myNum2){
+    alert("This is my first condition.\nEssa é minha primeira opção.");
+}
+else {
+    alert("This is my second condition.\nEssa é minha segunda opção.")
 }
