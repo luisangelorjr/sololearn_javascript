1fc667a461097dc82281928f73fdbebf5c590ffa
Luis Angelo Rodrigues Jr <luisangelorjr@gmail.com>
2019-03-01 17:40:48 -0300

    Lesson 03-07 'Math Operators' of '02-basic-concepts' is complete

--- 02-basic-concepts/js/main.js
+++ 02-basic-concepts/js/main.js
@@ -1,4 +1,23 @@
 var x = 10;
-alert("The value of variable 'x' is " + x);
-document.write("The value of variable 'x' is " + x);
-console.log("The value of variable 'x' is " + x);
+    alert("The value of variable 'x' is " + x);
+    document.write("The value of variable 'x' is " + x);
+    console.log("The value of variable 'x' is " + x);
+
+var price = 55.55;
+    console.log(price);
+
+var name = 'John! \n';
+var text = "My name is John Smith";
+    console.log(name + text);
+
+let isTrue = true;
+let isFalse = false;
+    console.log(isTrue);
+    console.log(isFalse);
+    console.log(isTrue + isFalse);
+
+var z = 10 + 5;
+    console.log(z);
+    console.log(eval("10 * 20 + 8"));
+    console.log(eval());
+
